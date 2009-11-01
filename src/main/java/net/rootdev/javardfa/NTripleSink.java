e47164cd5f629d53265a497a906b1eed858378d6
Damian Steer <pldms@mac.com>
2009-11-01 18:55:33 +0000

    Adding (c)

--- src/main/java/net/rootdev/javardfa/NTripleSink.java
+++ src/main/java/net/rootdev/javardfa/NTripleSink.java
@@ -1,6 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * (c) Copyright 2009 University of Bristol
+ * All rights reserved.
+ * [See end of file]
  */
 
 package net.rootdev.javardfa;
@@ -14,6 +15,8 @@ import java.util.regex.Pattern;
 
 
 /**
+ * A pretty ropey NTriple serialiser.
+ * Advantages: streams, no dependencies.
  *
  * @author pldms
  */
@@ -101,3 +104,30 @@ public class NTripleSink implements StatementSink {
         return String.format("\\u%08x", codepoint);
     }
 }
+
+/*
+ * (c) Copyright 2009 University of Bristol
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
--- src/main/java/net/rootdev/javardfa/ParserFactory.java
+++ src/main/java/net/rootdev/javardfa/ParserFactory.java
@@ -1,7 +1,9 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * (c) Copyright 2009 University of Bristol
+ * All rights reserved.
+ * [See end of file]
  */
+
 package net.rootdev.javardfa;
 
 import nu.validator.htmlparser.common.XmlViolationPolicy;
@@ -32,12 +34,21 @@ public class ParserFactory {
         }
     }
 
+    /**
+     *
+     * @return An XMLReader with validation turned off
+     * @throws SAXException
+     */
     public static XMLReader createNonvalidatingReader() throws SAXException {
         XMLReader reader = XMLReaderFactory.createXMLReader();
         reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
         return reader;
     }
 
+    /**
+     *
+     * @return An HTML 5 XMLReader set up to by fairly forgiving.
+     */
     public static XMLReader createHTML5Reader() {
         HtmlParser reader = new HtmlParser();
         reader.setXmlPolicy(XmlViolationPolicy.ALLOW);
@@ -46,6 +57,14 @@ public class ParserFactory {
         return reader;
     }
 
+    /**
+     * Get an XMLReader with rdfa parsing plumbed in to the sink.
+     *
+     * @param sink
+     * @param format
+     * @return
+     * @throws SAXException
+     */
     public static XMLReader createReaderForFormat(StatementSink sink, Format format) throws SAXException {
         XMLReader reader = getReader(format);
         Parser parser = getParser(format, sink);
@@ -73,3 +92,30 @@ public class ParserFactory {
         }
     }
 }
+
+/*
+ * (c) Copyright 2009 University of Bristol
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
