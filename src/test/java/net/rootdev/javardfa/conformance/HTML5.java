95b821d5dae60b29456a913b4b35d558d607d294
Damian Steer <pldms@colossus.danbri.org>
2010-02-05 14:47:54 +0000

    Adding new html 5 tests. Disabled currently (8 failures)

---
+++ src/test/java/net/rootdev/javardfa/conformance/HTML5.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.rootdev.javardfa.conformance;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Collections;
+import net.rootdev.javardfa.JenaStatementSink;
+import net.rootdev.javardfa.ParserFactory;
+import net.rootdev.javardfa.ParserFactory.Format;
+import org.junit.runners.Parameterized.Parameters;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ *
+ * @author pldms
+ */
+public class HTML5 extends RDFaConformance
+{
+    @Parameters
+    public static Collection<String[]> testFiles()
+            throws URISyntaxException, IOException {
+        if (true)
+            return Collections.EMPTY_LIST;
+        else
+        return RDFaConformance.
+                testFiles("http://rdfa.digitalbazaar.com/test-suite/test-cases/html5/html5-manifest.rdf");
+    }
+
+    public HTML5(String test, String title,
+            String purpose, String input, String query, String expected) {
+        super(test, title, purpose, input, query, expected);
+    }
+
+    @Override
+    public XMLReader getParser(Model model) throws SAXException {
+        return ParserFactory.createReaderForFormat(new JenaStatementSink(model), Format.HTML);
+    }
+}
