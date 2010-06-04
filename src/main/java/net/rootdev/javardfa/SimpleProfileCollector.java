4385c0c641e98da8aaa58627fccfdef7ebb82c45
Damian <d.steer@bris.ac.uk>
2010-06-04 17:39:09 +0100

    Tidy up

--- src/main/java/net/rootdev/javardfa/SimpleProfileCollector.java
+++ src/main/java/net/rootdev/javardfa/SimpleProfileCollector.java
@@ -9,6 +9,36 @@ package net.rootdev.javardfa;
  *
  * @author pldms
  */
-public class SimpleProfileCollector {
+public class SimpleProfileCollector implements ProfileCollector {
 
+    public void getProfile(String profileURI, EvalContext context) {
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    static class SimpleCollector implements StatementSink {
+        private final EvalContext context;
+
+        SimpleCollector(EvalContext context) {
+            this.context = context;
+        }
+
+        public void start() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        public void end() {}
+
+        // Doesn't use objects
+        public void addObject(String subject, String predicate, String object) {}
+
+        public void addLiteral(String subject, String predicate, String lex, String lang, String datatype) {
+            if (predicate.equals(ProfileCollector.prefix)) {
+
+            }
+        }
+
+        public void addPrefix(String prefix, String uri) {}
+
+    }
 }
