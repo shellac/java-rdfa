151a6c332140cf6e52b1fc9bd06fdcf7d022f5ef
Lars <heuer@semagia.com>
2010-10-15 00:21:41 +0200

    Doc / formatting issues

--- sesame/src/main/java/net/rootdev/javardfa/SesameRDFaParser.java
+++ sesame/src/main/java/net/rootdev/javardfa/SesameRDFaParser.java
@@ -1,34 +1,34 @@
 /*
-New BSD license: http://opensource.org/licenses/bsd-license.php
-
-Copyright (c) 2009 Sun Microsystems, Inc.
-901 San Antonio Road, Palo Alto, CA 94303 USA.
-All rights reserved.
-
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-- Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-- Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-- Neither the name of Sun Microsystems, Inc. nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+ *  New BSD license: http://opensource.org/licenses/bsd-license.php
+ *
+ *  Copyright (c) 2009 Sun Microsystems, Inc.
+ *  901 San Antonio Road, Palo Alto, CA 94303 USA.
+ *  All rights reserved.
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of Sun Microsystems, Inc. nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
  */
 package net.rootdev.javardfa;
 
@@ -63,143 +63,227 @@ import org.xml.sax.helpers.XMLReaderFactory;
  */
 public abstract class SesameRDFaParser extends RDFParserBase {
 
-   private static Logger log = LoggerFactory.getLogger(SesameRDFaParser.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SesameRDFaParser.class);
 
-   private XMLReader xmlReader;
+    private XMLReader xmlReader;
 
-   public static class HTMLRDFaParser extends SesameRDFaParser {
+    public static class HTMLRDFaParser extends SesameRDFaParser {
 
-      @Override
-      public XMLReader getReader() {
-         HtmlParser reader = new HtmlParser();
-         reader.setXmlPolicy(XmlViolationPolicy.ALLOW);
-         reader.setXmlnsPolicy(XmlViolationPolicy.ALLOW);
-         reader.setMappingLangToXmlLang(false);
-         return reader;
-      }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.SesameRDFaParser#getReader()
+         */
+        @Override
+        public XMLReader getReader() {
+            HtmlParser reader = new HtmlParser();
+            reader.setXmlPolicy(XmlViolationPolicy.ALLOW);
+            reader.setXmlnsPolicy(XmlViolationPolicy.ALLOW);
+            reader.setMappingLangToXmlLang(false);
+            return reader;
+        }
 
-      @Override
-      public void initParser(Parser parser) {
-         parser.enable(Setting.ManualNamespaces);
-      }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.SesameRDFaParser#initParser(net.rootdev.javardfa.Parser)
+         */
+        @Override
+        public void initParser(Parser parser) {
+            parser.enable(Setting.ManualNamespaces);
+        }
 
-      @Override
-      public RDFFormat getRDFFormat() {
-         return RDFaHtmlParserFactory.rdfa_html_Format;
-      }
-   }
+        /* (non-Javadoc)
+         * @see org.openrdf.rio.RDFParser#getRDFFormat()
+         */
+        @Override
+        public RDFFormat getRDFFormat() {
+            return RDFaHtmlParserFactory.rdfa_html_Format;
+        }
+    }
 
-   public static class XHTMLRDFaParser extends SesameRDFaParser {
+    public static class XHTMLRDFaParser extends SesameRDFaParser {
 
-      @Override
-      public XMLReader getReader() throws SAXException {
-         XMLReader reader = XMLReaderFactory.createXMLReader();
-         reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-         return reader;
-      }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.SesameRDFaParser#getReader()
+         */
+        @Override
+        public XMLReader getReader() throws SAXException {
+            XMLReader reader = XMLReaderFactory.createXMLReader();
+            reader.setFeature(
+                    "http://apache.org/xml/features/nonvalidating/load-external-dtd",
+                    false);
+            return reader;
+        }
 
-      public RDFFormat getRDFFormat() {
-         return RDFaXHtmlParserFactory.rdfa_xhtml_Format;
-      }
-   }
+        /* (non-Javadoc)
+         * @see org.openrdf.rio.RDFParser#getRDFFormat()
+         */
+        public RDFFormat getRDFFormat() {
+            return RDFaXHtmlParserFactory.rdfa_xhtml_Format;
+        }
+    }
 
-   public void setReader(XMLReader reader) {
-      this.xmlReader = reader;
-   }
+    /**
+     * 
+     *
+     * @param reader
+     */
+    public void setReader(XMLReader reader) {
+        this.xmlReader = reader;
+    }
 
-   protected XMLReader getReader() throws SAXException {
-      return xmlReader;
-   }
+    /**
+     * 
+     *
+     * @return
+     * @throws SAXException
+     */
+    protected XMLReader getReader() throws SAXException {
+        return xmlReader;
+    }
 
-   protected void initParser(Parser parser) {
-   }
+    /**
+     * 
+     *
+     * @param parser
+     */
+    protected void initParser(Parser parser) {
+    }
 
-   public void parse(InputStream in, String baseURI) throws IOException, RDFParseException, RDFHandlerException {
-      parse(new InputSource(in), baseURI);
-   }
+    /* (non-Javadoc)
+     * @see org.openrdf.rio.RDFParser#parse(java.io.InputStream, java.lang.String)
+     */
+    @Override
+    public void parse(InputStream in, String baseURI) throws IOException,
+            RDFParseException, RDFHandlerException {
+        parse(new InputSource(in), baseURI);
+    }
 
-   public void parse(Reader reader, String baseURI) throws IOException, RDFParseException, RDFHandlerException {
-      parse(new InputSource(reader), baseURI);
-   }
+    /* (non-Javadoc)
+     * @see org.openrdf.rio.RDFParser#parse(java.io.Reader, java.lang.String)
+     */
+    @Override
+    public void parse(Reader reader, String baseURI) throws IOException,
+            RDFParseException, RDFHandlerException {
+        parse(new InputSource(reader), baseURI);
+    }
 
-   private void parse(InputSource in, String baseURI) throws IOException {
-      Parser parser = new Parser(new SesameStatementSink());
-      parser.setBase(baseURI);
-      initParser(parser);
-      try {
-         XMLReader xreader = getReader();
-         xreader.setContentHandler(parser);
-         xreader.parse(in);
-      } catch (SAXException ex) {
-         throw new RuntimeException("SAX Error when parsing", ex);
-      }
-   }
+    /**
+     * 
+     *
+     * @param in
+     * @param baseURI
+     * @throws IOException
+     */
+    private void parse(InputSource in, String baseURI) throws IOException {
+        Parser parser = new Parser(new SesameStatementSink());
+        parser.setBase(baseURI);
+        initParser(parser);
+        try {
+            XMLReader xreader = getReader();
+            xreader.setContentHandler(parser);
+            xreader.parse(in);
+        }
+        catch (SAXException ex) {
+            throw new IOException("SAX Error when parsing", ex);
+        }
+    }
 
+    /**
+     * 
+     * 
+     */
+    private class SesameStatementSink implements StatementSink {
 
-   private class SesameStatementSink implements StatementSink {
+        private Map<String, Resource> bnodeLookup;
 
-       private Map<String, Resource> bnodeLookup;
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#start()
+         */
+        @Override
+        public void start() {
+            bnodeLookup = new HashMap<String, Resource>();
+        }
 
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#end()
+         */
+        @Override
+        public void end() {
+            bnodeLookup = null;
+        }
 
-       //@Override
-       public void start() {
-          bnodeLookup = new HashMap<String, Resource>();
-       }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#addObject(java.lang.String, java.lang.String, java.lang.String)
+         */
+        @Override
+        public void addObject(String subject, String predicate, String object) {
+            try {
+                Resource s = getResource(subject);
+                Resource o = getResource(object);
+                rdfHandler.handleStatement(createStatement(s,
+                        createURI(predicate), o));
+            }
+            catch (OpenRDFException ex) {
+                LOG.warn("Unexpected exception", ex);
+            }
+        }
 
-       //@Override
-       public void end() {
-          bnodeLookup = null;
-       }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#addLiteral(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+         */
+        @Override
+        public void addLiteral(String subject, String predicate, String lex,
+                String lang, String datatype) {
+            try {
+                Resource s = getResource(subject);
+                URI p = createURI(predicate);
+                Literal o = createLiteral(lex, lang,
+                        datatype != null ? createURI(datatype) : null);
+                rdfHandler.handleStatement(createStatement(s, p, o));
+            }
+            catch (OpenRDFException ex) {
+                LOG.warn("Unexpected exception", ex);
+            }
+        }
 
-       //@Override
-       public void addObject(String subject, String predicate, String object) {
-          try {
-             Resource s = getResource(subject);
-             Resource o = getResource(object);
-             rdfHandler.handleStatement(createStatement(s, createURI(predicate), o));
-          } catch (OpenRDFException ex) {
-              log.warn("Unexpected exception", ex);
-          }
-       }
+        /**
+         * 
+         *
+         * @param res
+         * @return
+         * @throws RDFParseException
+         */
+        private Resource getResource(String res) throws RDFParseException {
+            if (res.startsWith("_:")) {
+                if (bnodeLookup.containsKey(res)) {
+                    return bnodeLookup.get(res);
+                }
+                Resource bnode = createBNode();
+                bnodeLookup.put(res, bnode);
+                return bnode;
+            }
+            else {
+                return createURI(res);
+            }
+        }
 
-       @Override
-       public void addLiteral(String subject, String predicate, String lex, String lang, String datatype) {
-          try {
-             Resource s = getResource(subject);
-             URI p = createURI(predicate);
-             Literal o = createLiteral(lex, lang, datatype != null ? createURI(datatype) : null);
-             rdfHandler.handleStatement(createStatement(s, p, o));
-          } catch (OpenRDFException ex) {
-              log.warn("Unexpected exception", ex);
-          }
-       }
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#addPrefix(java.lang.String, java.lang.String)
+         */
+        @Override
+        public void addPrefix(String prefix, String uri) {
+            try {
+                rdfHandler.handleNamespace(prefix, uri);
+            }
+            catch (RDFHandlerException ex) {
+                LOG.warn("Unexpected exception", ex);
+            }
+        }
 
-       private Resource getResource(String res) throws RDFParseException {
-          if (res.startsWith("_:")) {
-             if (bnodeLookup.containsKey(res)) {
-                return bnodeLookup.get(res);
-             }
-             Resource bnode = createBNode();
-             bnodeLookup.put(res, bnode);
-             return bnode;
-          } else {
-             return createURI(res);
-          }
-       }
-
-       @Override
-       public void addPrefix(String prefix, String uri) {
-           try {
-             rdfHandler.handleNamespace(prefix, uri);
-          } catch (RDFHandlerException ex) {
-              log.warn("Unexpected exception", ex);
-          }
-       }
-
-       @Override
-       public void setBase(String base) {
-           // TODO Auto-generated method stub
-       }
-
+        /* (non-Javadoc)
+         * @see net.rootdev.javardfa.StatementSink#setBase(java.lang.String)
+         */
+        @Override
+        public void setBase(String base) {
+            // noop.
+        }
     }
 }
